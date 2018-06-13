d94198f18fddd5a2dfa5b763c0ac31fc375b6816
Kilian <Kilian@muelken.com>
2018-06-13 20:31:22 +0200

    FusionEKF.cpp filled

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -37,7 +37,13 @@ FusionEKF::FusionEKF() {
     * Set the process and measurement noises
   */
 
+  // measurement matrix
+  H_laser_ << 1, 0, 0, 0,
+		0, 1, 0, 0;
 
+  // Set the process and measurement noises
+  noise_ax = 9;
+  noise_ay = 9;
 }
 
 /**
@@ -56,7 +62,6 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     TODO:
       * Initialize the state ekf_.x_ with the first measurement.
       * Create the covariance matrix.
-      * Remember: you'll need to convert radar from polar to cartesian coordinates.
     */
     // first measurement
     cout << "EKF: " << endl;
@@ -64,14 +69,21 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.x_ << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-      /**
-      Convert radar from polar to cartesian coordinates and initialize state.
-      */
+      // Convert radar from polar to cartesian coordinates and initialize state.
+	  // Source: https://www.mathsisfun.com/polar-cartesian-coordinates.html
+		float rho = measurement_pack.raw_measurements_(0);
+		float phi = measurement_pack.raw_measurements_(1);
+		ekf_.x_(0) = rho * cos(phi); // px
+		ekf_.x_(1) = rho * sin(phi); // py
+		ekf_.x_(2) = 0; // vx
+		ekf_.x_(3) = 0; // vy
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      /**
-      Initialize state.
-      */
+		//Initialize state
+		ekf_.x_(0) = measurement_pack.raw_measurements_(0); // px
+		ekf_.x_(1) = measurement_pack.raw_measurements_(1); // py
+		ekf_.x_(2) = 0; // vx
+		ekf_.x_(3) = 0; // vy
     }
 
     // done initializing, no need to predict or update
@@ -82,31 +94,47 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   /*****************************************************************************
    *  Prediction
    ****************************************************************************/
+  // Update the state transition matrix F according to the new elapsed time. - Time is measured in seconds.
+  // compute the time elapsed between the current and previous measurements
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+  previous_timestamp_ = measurement_pack.timestamp_;
 
-  /**
-   TODO:
-     * Update the state transition matrix F according to the new elapsed time.
-      - Time is measured in seconds.
-     * Update the process noise covariance matrix.
-     * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
-   */
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+
+  // Modify the F matrix so that the time is integrated
+  ekf_.F_ << 1, 0, dt, 0,
+	  0, 1, 0, dt,
+	  0, 0, 1, 0,
+	  0, 0, 0, 1;
+
+   // Update the process noise covariance matrix Q
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ << (dt_4*noise_ax / 4), 0, (dt_3*noise_ax / 2), 0,
+	  0, (dt_4*noise_ay / 4), 0, (dt_3*noise_ay / 2),
+	  (dt_3*noise_ax / 2), 0, (dt_2*noise_ax), 0,
+	  0, (dt_3*noise_ay / 2), 0, (dt_2*noise_ay);
 
   ekf_.Predict();
 
   /*****************************************************************************
    *  Update
    ****************************************************************************/
-
-  /**
-   TODO:
-     * Use the sensor type to perform the update step.
-     * Update the state and covariance matrices.
-   */
-
-  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    // Update the state and covariance matrices. 
+   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-  } else {
+	  Tools t;
+	  ekf_.H_ = t.CalculateJacobian(ekf_.x_);
+	  ekf_.R_ = R_radar_;
+	  ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+
+	} 
+  else {
     // Laser updates
+	  ekf_.R_ = R_laser_;
+	  ekf_.H_ = H_laser_;
+	  ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
